e44b7b27499a113d79c2f3f6defeecd25a95ee95
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-18 20:02:13 -0500

    create MyLinkedList

---
+++ MyList/src/MyList/MyLinkedList.java
@@ -0,0 +1,144 @@
+package MyList;
+
+public class MyLinkedList {
+
+	MyNode first;
+	MyNode last;
+	int size;
+	
+	public String getFirst() {
+		return first.element;
+	}
+	
+	public String getLast() {
+		return last.getElement();
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	// O(1)
+	public void addFirst(String s) {
+		MyNode newNode = new MyNode(s);
+		if (isEmpty()) {
+			last = newNode;
+		} else {
+			newNode.setNext(first);
+		}
+		first = newNode;
+		size++;
+	}
+	
+	// O(1)
+	public void addLast(String s) {
+		MyNode newNode = new MyNode(s);
+		if(isEmpty()){
+			first = newNode;
+		}else{
+			last.setNext(newNode);
+
+		}
+		last = newNode;
+		size++;
+	}
+	
+	public void removeFirst(){
+		if(!isEmpty()){
+			String temp = first.getElement();
+			if(first == last){
+				first = null;
+				last = null;
+			}else{
+				first = first.getNext();
+			}
+			size--;
+			System.out.println(temp +" is removed.");
+		}else{
+			System.out.println("The list is empty");
+		}
+	}
+	
+	// O(n)
+	public void removeLast(){
+		if(!isEmpty()){
+			String temp = last.getElement();
+			if(last == first){
+				last = first = null;
+			}else{
+				MyNode prev = first;
+				while(prev.getNext() != last){
+					prev = prev.getNext();
+				}
+				prev.setNext(null);
+				last = prev;
+			}
+			size--;
+			System.out.println(temp + " is removed.");
+		}else{
+			System.out.println("The list is empty");
+		}
+	}
+	
+	public boolean search(String key){
+		MyNode pointer = first;
+		while(pointer != null && pointer.getNext().equals(key)){
+			pointer = pointer.getNext();
+		}
+		return pointer != null;
+	}
+	
+	public void remove(String key) {
+		if (search(key)) {
+			MyNode prev = first;
+			MyNode curr = first;
+			while (curr != null && !curr.getElement().equals(key)) {
+				prev = curr;
+				curr = curr.getNext();
+			}
+			if (curr == first) {
+				removeFirst();
+			} else if (curr == last) {
+				removeLast();
+			} else {
+				prev.setNext(curr.getNext());
+				size--;
+				System.out.println(key + " is removed.");
+			}
+		} else {
+			System.out.println(key + " is not in the list.");
+		}
+	}
+	
+	public void print(){
+		MyNode pointer = first;
+		if(!isEmpty()) {
+			System.out.println(" | ");
+			while (pointer != null) {
+				System.out.println(pointer.getElement() + " | ");
+				pointer = pointer.getNext();
+			}
+			System.out.println();
+		}
+	}
+}
+
+class MyNode{
+	String element;
+	MyNode next;
+	MyNode(String element){
+		this.element = element;
+	}
+	String getElement(){
+		return element;
+	}
+	public void setNext(MyNode first) {
+	}
+	public MyNode getNext() {
+		return next;
+	}
+}
