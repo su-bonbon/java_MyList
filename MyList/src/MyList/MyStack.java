8137d71fa69bf65a7ee910894863b0c6788e1f11
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-23 15:18:15 -0500

    implement basic methods

--- MyList/src/MyList/MyStack.java
+++ MyList/src/MyList/MyStack.java
@@ -1,5 +1,20 @@
 package MyList;
 
 public class MyStack {
-
+	
+	MyLinkedList stack = new MyLinkedList();
+	
+	public void push(String s) {
+		stack.addFirst(s);
+	}
+	
+	public void pop() {
+		stack.removeFirst();
+	}
+	
+	public String peek() {
+		return stack.getFirst();
+	}
+	
+	
 }
