a3a5699aaf4a54b57fa0265b844447eaf8ea3f0c
Sujin Lee <sujinbonnaud@gmail.com>
2023-10-23 15:22:52 -0500

    create queue class

---
+++ MyList/src/MyList/MyQueue.java
@@ -0,0 +1,5 @@
+package MyList;
+
+public class MyQueue {
+	
+}
--- MyList/src/MyList/MyStack.java
+++ MyList/src/MyList/MyStack.java
@@ -1,20 +1,34 @@
 package MyList;
 
 public class MyStack {
+	int top;
 	
 	MyLinkedList stack = new MyLinkedList();
 	
 	public void push(String s) {
 		stack.addFirst(s);
+		top = getSize();
 	}
 	
 	public void pop() {
 		stack.removeFirst();
+		top = getSize();
 	}
 	
 	public String peek() {
 		return stack.getFirst();
 	}
 	
+	public boolean isEmpty() {
+		return stack.isEmpty();
+	}
+	
+	public int getSize() {
+		return stack.getSize();
+	}
+	
+	public boolean search (String key) {
+		return stack.search(key);
+	}
 	
 }
